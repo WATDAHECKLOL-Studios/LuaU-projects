1e279fec46cdc9bde650da26dfc5d1c7bdac266f
DrBIoop <144206470+SCPFBluesky@users.noreply.github.com>
2025-05-09 01:40:31 -0500

    Create Methods.lua

---
+++ Code/MISC/Methods.lua
@@ -0,0 +1,72 @@
+local getrawmetatable = debug.getmetatable;
+local newcclosure = function(func)
+	return coroutine.wrap(function(...)
+		local params = {...};
+		local nuhuh = function()
+			params = nil;
+		end;
+		while 1 do
+			params = {coroutine.yield(func(unpack(params)), coroutine.wrap(nuhuh)())};
+		end;
+	end);
+end;
+
+local hookmetamethod = newcclosure(function(Object, Metamethod, Closure)
+	local Metatable = getrawmetatable(Object);
+	local Old = rawget(Metatable, Metamethod);
+
+	assert(Old, Metamethod.. " Is not a valid metamethod of ".. tostring(Object));
+	rawset(Metatable, Metamethod, Closure);
+
+	return Old;
+end);
+
+local getnamecallmethod;
+
+local namecallatom = newcclosure(function(self, index, ...)
+	assert(self);
+
+
+	getnamecallmethod = newcclosure(function()
+		return index
+	end);
+
+	if getrawmetatable(self) and getrawmetatable(self).__namecall then
+		getrawmetatable(self).__namecall(self, ...);
+	end;
+
+	getnamecallmethod = function() 
+		return nil;
+	end;
+
+	namecallmethods = {};
+end);
+
+local typeof = newcclosure(function(Object)
+	if getrawmetatable(Object) and getrawmetatable(Object).__type then
+		return getrawmetatable(Object).__type;
+	end;
+
+	return type(Object);
+end);
+
+local mine = setmetatable({}, {
+	__index = function(Self, Index, ...)
+		return rawget(Self, Index) or function(...)
+            namecallatom(Self, Index, ...);
+		end;
+
+
+
+	end;
+
+	__namecall = function(Self, ...)
+		print("namecall metamethod invoked with", getnamecallmethod(), "with arugments of", ...);
+	end;
+
+    
+	__metatable = "The metatable is locked";
+	__type = "Instance";
+});
+
+table.foreach(getrawmetatable(mine), print);
